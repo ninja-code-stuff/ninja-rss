b84644fc0efe7ab14eb8e3426946961b1c8f798e
sak96 <26397224+sak96@users.noreply.github.com>
2020-12-19 13:30:36 +0530

    Add Argument parsing logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#[macro_use]
+extern crate clap;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = clap_app!(ninja_rss =>
+        (version: crate_version!())
+        (about: crate_description!())
+        (@setting VersionlessSubcommands)
+        (@subcommand add =>
+         (about: "add rss url")
+         (@arg url: +required "url to be added")
+        )
+        (@subcommand list =>
+         (about: "list rss url")
+        )
+    )
+    .get_matches();
+
+    match matches.subcommand() {
+        ("add", Some(arg)) => { println!("add reached with url {}", arg.value_of("url").unwrap())}
+        ("list", Some(_)) => {println!("list reached") }
+        _ => {
+            eprintln!("{}", matches.usage());
+        }
+    }
 }
+
+
