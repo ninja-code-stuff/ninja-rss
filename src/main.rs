04254e907f375e5fd8b430fa9687a44ae7015128
sak96 <26397224+sak96@users.noreply.github.com>
2020-12-21 11:59:57 +0530

    Add delete subcommand

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,10 @@ fn create_app() -> App<'static, 'static> {
          (about: "add rss url")
          (@arg url: +required "url to be added")
         )
+        (@subcommand del =>
+         (about: "delete rss by id")
+         (@arg id: +required "id to be deleted")
+        )
         (@subcommand list =>
          (about: "list rss url")
         )
@@ -28,6 +32,12 @@ fn main() -> Result<(), Box<dyn Error>> {
                 .value_of("url")
                 .ok_or("could not get url argument for add")?;
             ninja_rss::rss_manager::get_rss_manager()?.add(url)?;
+        }
+        ("del", Some(arg)) => {
+            let id = arg
+                .value_of("id")
+                .ok_or("could not get url argument for add")?;
+            ninja_rss::rss_manager::get_rss_manager()?.delete(id.parse().unwrap())?;
         }
         ("list", Some(_)) => {
             println!("{:#?}", ninja_rss::rss_manager::get_rss_manager()?.list()?);
@@ -44,9 +54,15 @@ mod tests {
 
     use super::*;
 
+    // Note: get_matches_from_safe provides and Result
+    //       unwrap() is used to fail the tests for err cases
+    //       in prod helpful message is shown by not using safe
+
     #[test]
     fn test_add() {
-        let matches = create_app().get_matches_from(vec!["ninja_rss", "add", "url"]);
+        let matches = create_app()
+            .get_matches_from_safe(vec!["ninja_rss", "add", "url"])
+            .unwrap();
         assert!(matches.subcommand_matches("add").is_some());
         let args = matches.subcommand_matches("add").unwrap();
         assert_eq!(args.value_of("url"), Some("url"));
@@ -54,7 +70,27 @@ mod tests {
 
     #[test]
     fn test_list() {
-        let matches = create_app().get_matches_from(vec!["ninja_rss", "list"]);
+        let matches = create_app()
+            .get_matches_from_safe(vec!["ninja_rss", "list"])
+            .unwrap();
         assert!(matches.subcommand_matches("list").is_some());
+    }
+
+    #[test]
+    fn test_delete() {
+        let matches = create_app()
+            .get_matches_from_safe(vec!["ninja_rss", "del", "3"])
+            .unwrap();
+        assert!(matches.subcommand_matches("del").is_some());
+        let args = matches.subcommand_matches("del").unwrap();
+        assert_eq!(args.value_of("id"), Some("3"));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_wrong_case() {
+        create_app()
+            .get_matches_from_safe(vec!["ninja_rss", "wrong_case"])
+            .unwrap();
     }
 }
