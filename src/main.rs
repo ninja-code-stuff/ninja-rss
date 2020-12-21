eb85ab4d27a3248387c7e05851d5f1cf96ad2047
sak96 <26397224+sak96@users.noreply.github.com>
2020-12-21 12:53:15 +0530

    Use structopt instead of clap for type conversion

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,29 @@
 use std::error::Error;
 
-use clap::App;
+use structopt::clap::AppSettings;
+use structopt::StructOpt;
 
-#[macro_use]
-extern crate clap;
-
-fn create_app() -> App<'static, 'static> {
-    clap_app!(ninja_rss =>
-        (version: crate_version!())
-        (about: crate_description!())
-        (@setting VersionlessSubcommands)
-        (@subcommand add =>
-         (about: "add rss url")
-         (@arg url: +required "url to be added")
-        )
-        (@subcommand del =>
-         (about: "delete rss by id")
-         (@arg id: +required "id to be deleted")
-        )
-        (@subcommand list =>
-         (about: "list rss url")
-        )
-    )
+#[derive(Debug, PartialEq, StructOpt)]
+#[structopt(about, global_settings(&[AppSettings::ColoredHelp, AppSettings::VersionlessSubcommands]))]
+enum Opt {
+    #[structopt(no_version, about = "Add rss feed by url")]
+    Add { url: String },
+    #[structopt(no_version, about = "Delete rss feed by id")]
+    Del { id: i32 },
+    #[structopt(no_version, about = "List rss feed")]
+    List,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let matches = create_app().get_matches();
-    match matches.subcommand() {
-        ("add", Some(arg)) => {
-            let url = arg
-                .value_of("url")
-                .ok_or("could not get url argument for add")?;
-            ninja_rss::rss_manager::get_rss_manager()?.add(url)?;
+    match Opt::from_args() {
+        Opt::Add { url } => {
+            ninja_rss::rss_manager::get_rss_manager()?.add(&url)?;
         }
-        ("del", Some(arg)) => {
-            let id = arg
-                .value_of("id")
-                .ok_or("could not get url argument for add")?;
-            ninja_rss::rss_manager::get_rss_manager()?.delete(id.parse().unwrap())?;
+        Opt::Del { id } => {
+            ninja_rss::rss_manager::get_rss_manager()?.delete(id)?;
         }
-        ("list", Some(_)) => {
+        Opt::List => {
             println!("{:#?}", ninja_rss::rss_manager::get_rss_manager()?.list()?);
-        }
-        _ => {
-            eprintln!("{}", matches.usage());
         }
     }
     Ok(())
@@ -54,43 +34,37 @@ mod tests {
 
     use super::*;
 
-    // Note: get_matches_from_safe provides and Result
+    // Note: from_iter_safe provides Result
     //       unwrap() is used to fail the tests for err cases
     //       in prod helpful message is shown by not using safe
 
     #[test]
     fn test_add() {
-        let matches = create_app()
-            .get_matches_from_safe(vec!["ninja_rss", "add", "url"])
-            .unwrap();
-        assert!(matches.subcommand_matches("add").is_some());
-        let args = matches.subcommand_matches("add").unwrap();
-        assert_eq!(args.value_of("url"), Some("url"));
+        assert_eq!(
+            Opt::from_iter_safe(vec!["ninja_rss", "add", "url"]).unwrap(),
+            Opt::Add { url: "url".into() }
+        );
     }
 
     #[test]
     fn test_list() {
-        let matches = create_app()
-            .get_matches_from_safe(vec!["ninja_rss", "list"])
-            .unwrap();
-        assert!(matches.subcommand_matches("list").is_some());
-    }
-
-    #[test]
-    fn test_delete() {
-        let matches = create_app()
-            .get_matches_from_safe(vec!["ninja_rss", "del", "3"])
-            .unwrap();
-        assert!(matches.subcommand_matches("del").is_some());
-        let args = matches.subcommand_matches("del").unwrap();
-        assert_eq!(args.value_of("id"), Some("3"));
+        assert_eq!(
+            Opt::from_iter_safe(vec!["ninja_rss", "list"]).unwrap(),
+            Opt::List
+        );
     }
 
     #[test]
     #[should_panic]
-    fn test_wrong_case() {
-        create_app()
-            .get_matches_from_safe(vec!["ninja_rss", "wrong_case"])
-            .unwrap();
+    fn test_list_with_args() {
+        Opt::from_iter_safe(vec!["ninja_rss", "list", "args_added"]).unwrap();
+    }
+
+    #[test]
+    fn test_delete() {
+        assert_eq!(
+            Opt::from_iter_safe(vec!["ninja_rss", "del", "3"]).unwrap(),
+            Opt::Del { id: 3 }
+        );
     }
 }
